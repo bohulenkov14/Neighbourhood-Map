a4220232d72cf2ee73ff93a9800eaa2077d6826e
bohulenkov.a <bohulenkov14@gmail.com>
2015-11-06 14:55:44 +0300

    feat: Added additional info for infoWindows

--- js/app.js
+++ js/app.js
@@ -5,9 +5,9 @@ var ApplicationViewModel = function() {
 	var currentMarkers = [];
 	var defaultNeighborhood = "Moscow";
 	var myLatLng = {lat: -34.397, lng: 150.644};
-	
-	self.neighborhood = ko.observable(defaultNeighborhood); 
-	
+
+	self.neighborhood = ko.observable(defaultNeighborhood);
+
 	var initMap = function() {
 		// Create a map object and specify the DOM element for display.
 		map = new google.maps.Map(document.getElementById('map'), {
@@ -17,34 +17,39 @@ var ApplicationViewModel = function() {
 			streetViewControl: false
 		});
 	}
-	
+
 	var addMarkerToMap = function(location) {
-  
+
 		var marker = new google.maps.Marker({
 			position: { lat:location.lantitude , lng:location.longtitide },
 			map: map
 		});
-		
+
 		var img;
 		if (location.thumbnail !== '')
 			img = '<img src="' + location.thumbnail + '" class="thumbnail">';
 		else
 			img = '';
-			
+
 		var contentString = '<div id="content">'+
-			'<div id="siteNotice">'+
-			'</div>'+
-			'<div id="bodyContent">'+
-			'<h3>' + location.name + '</h3>' +
+			'<h2>' + location.name + '</h2>' +
+			'<div class="img-contacts">'+
+			'<div class="img">' +
 			img +
+			'</div>' +
+			'<div class="contacts">' +
+			'<div><span style="font-weight:bold;" class="adress">Address: </span>' + location.address + '</div>' +
+			'<div><span style="font-weight:bold;" class="twitter">Rating: </span>' + location.rating + '</div>' +
+			'<div><a class="fa fa-foursquare" href="'+ location.foursquareLink + '"></a></div>' +
+			'</div>' +
+			'</div>' +
 			'<p>' + location.caption + '</p>'+
-			'</div>'+
 			'</div>';
-			
+
 		var infowindow = new google.maps.InfoWindow({
 			content: contentString
 		});
-		
+
 		marker.addListener('click', (function(infWnd ) {
 			return function() {
 				if (currentOpenedInfowindow)
@@ -53,13 +58,13 @@ var ApplicationViewModel = function() {
 				currentOpenedInfowindow = infWnd;
 			};
 		})(infowindow));
-		
+
 		currentMarkers.push(marker);
 	}
 	initMap();
-	
+
 	var foursquareRequestCompleted = function( data ) {
-		
+
 		var results = data.response.group.results;
 		if (results) {
 			if (results.length > 0) {
@@ -69,51 +74,54 @@ var ApplicationViewModel = function() {
 				map.setZoom(10);
 				map.panTo(mapZoom);
 			}
-			 
+
 			results.forEach(function( item ) {
 				var latitude = item.venue.location.lat;
 				var longtitude = item.venue.location.lng;
 				var name = item.venue.name;
 				var caption;
-				if (item.snippets.items.length > 0 && 
+				if (item.snippets.items.length > 0 &&
 					item.snippets.items[0].hasOwnProperty('detail') &&
 					item.snippets.items[0].detail.hasOwnProperty('object') &&
 					item.snippets.items[0].detail.object.hasOwnProperty('text'))
 					caption = item.snippets.items[0].detail.object.text;
 				else
 					caption = '';
-				
+
 				var photo;
 				if (item.hasOwnProperty('photo'))
 					photo = item.photo.prefix + '100x100' + item.photo.suffix;
 				else
 					photo = '';
-				
-	
-				var loc = new Location(latitude, longtitude, name, caption, photo);
+
+				var address = item.venue.location.formattedAddress[0];
+				var rating = item.venue.rating;
+				var url = item.venue.canonicalUrl;
+
+				var loc = new Location(latitude, longtitude, name, caption, photo, address, rating, url);
 				addMarkerToMap(loc);
 			});
 		}
-	} 
-	
+	}
+
 	var clearMarkers = function() {
 		for ( var i = 0; i < currentMarkers.length; ++i ) {
 			currentMarkers[i].setMap(null);
 		}
 		currentMarkers.length = 0;
 	}
-	
+
 	self.computedMarkers = ko.computed(function() {
 		if (self.neighborhood() !== '') {
 			clearMarkers();
 			var foursquareLink = 'https://api.foursquare.com/v2/search/recommendations?m=foursquare&near='+self.neighborhood()+'&oauth_token=MRX15WXU5C42TF0ZI12BNY4GFIFNBYFVMJWXITDDTOUFYIRL&v=20151103';
-			//var foursquareLink = 'https://api.foursquare.com/v2/venues/explore?near='+encodeURIComponent(self.neighborhood())+'&oauth_token=5WJZ5GSQURT4YEG251H42KKKOWUNQXS5EORP2HGGVO4B14AB&v=20151103'; 
+			//var foursquareLink = 'https://api.foursquare.com/v2/venues/explore?near='+encodeURIComponent(self.neighborhood())+'&oauth_token=5WJZ5GSQURT4YEG251H42KKKOWUNQXS5EORP2HGGVO4B14AB&v=20151103';
 			$.getJSON(foursquareLink, foursquareRequestCompleted)
 			.fail(function() {})
 	    }
 	});
 
-	
+
 
 }
 
--- js/location.js
+++ js/location.js
@@ -1,7 +1,10 @@
-var Location = function(latitude, longtitude, name, caption, thumbnail) {
-	this.lantitude = latitude;
-	this.longtitide = longtitude;
-	this.name = name;
-	this.caption = caption;
-	this.thumbnail = thumbnail;
+var Location = function(latitude, longtitude, name, caption, thumbnail, address, rating, foursquareLink) {
+  this.lantitude = latitude;
+  this.longtitide = longtitude;
+  this.name = name;
+  this.caption = caption;
+  this.thumbnail = thumbnail;
+  this.address = address;
+  this.rating = rating;
+  this.foursquareLink = foursquareLink;
 }
